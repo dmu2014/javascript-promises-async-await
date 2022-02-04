30c8e01bab1608b83fec4b4b459fcec74c50a9a5
dmu2014 <dishank2007@gmail.com>
2022-02-04 07:42:41 -0500

    module 3 code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import { fetchWithTimeout } from './services';
+
+let movies = require('./data/movies.json');
+
 export function fetchMovies(){
-    
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+let moviePromise = fetchMovies();
+moviePromise.then((results) => {console.log(results)});
+
