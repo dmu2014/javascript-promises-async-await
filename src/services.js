78b35d148de9c41914974f34d7fb6ba798781a4c
dmu2014 <dishank2007@gmail.com>
2022-02-04 11:44:40 -0500

    module 6 code

--- src/services.js
+++ src/services.js
@@ -25,4 +25,15 @@ export async function asyncFetchMovies(){
     catch(error){
         console.log(error);
     }
-} 
+}
+
+export async function asyncFetchBooks(){
+    try{
+        const response = await fetch("/data/books.json");
+        const results = await response.json();
+        return results;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
